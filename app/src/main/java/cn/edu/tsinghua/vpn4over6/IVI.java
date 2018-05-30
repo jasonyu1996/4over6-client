4e2982df65365d63d31b8082756e2cea72c095d6
xinwei <hexw15@mails.tsinghua.edu.cn>
2018-05-31 00:26:46 +0800

    fix frontend display

--- app/src/main/java/cn/edu/tsinghua/vpn4over6/IVI.java
+++ app/src/main/java/cn/edu/tsinghua/vpn4over6/IVI.java
@@ -17,6 +17,7 @@ import android.os.Environment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,47 +30,43 @@ import android.util.Log;
 public class IVI extends AppCompatActivity {
 
     public Timer mTimer = new Timer();
-
-    private TextView ipv6Addr;
-    private TextView ipv4Addr;
-    private TextView uploadInfo;
-    private TextView downloadInfo;
-    private TextView runTime;
-    private FloatingActionButton mFab;
+    private TextView textView3, textView5, textView8, textView10, textView12;
 
     private int running = 0; //服务是否已开启（1）的标志
     private int flag = 0; //决定读取ip管道信息（0）或读取流量管道信息（1）的标志
+    private int mhour = 0;
+    private int mminute = 0;
+    private int msecond = 0;
 
     private byte[] readBuf;
     private byte[] writeBuf;
 
-    public Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == 1) {
-                renewUI();
-            }
-            super.handleMessage(msg);
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ipv6Addr = findViewById(R.id.textView3);
-        ipv4Addr = findViewById(R.id.textView5);
-        uploadInfo = findViewById(R.id.textView8);
-        downloadInfo = findViewById(R.id.textView10);
-        runTime = findViewById(R.id.textView12);
-        VPNBackend vpnBackend = new VPNBackend();
-        Log.i("MainActivity", "result: " + vpnBackend.startThread());
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ivi);
 
+        //View view = this.getLayoutInflater().inflate(R.layout.content_ivi, null);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView5 = (TextView) findViewById(R.id.textView5);
+        textView8 = (TextView) findViewById(R.id.textView8);
+        textView10 = (TextView) findViewById(R.id.textView10);
+        textView12 = (TextView) findViewById(R.id.textView12);
+        textView3.setText("尚未连接");
+        textView5.setText("尚未连接");
+        textView8.setText("0 M ↑ 0 MB/s");
+        textView10.setText("0 M ↓ 0 MB/s");
+        textView12.setText("00:00:00");
+        VPNBackend vpnBackend = new VPNBackend();
+        Log.i("MainActivity", "result: " + vpnBackend.startThread());
+
+        View converView = this.getLayoutInflater().inflate(R.layout.activity_ivi,null);
+
         Toolbar mToolBar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolBar);
 
-        mFab = findViewById(R.id.fab);
+        FloatingActionButton mFab = findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //点击悬浮按钮，开启或停止服务
@@ -85,6 +82,9 @@ public class IVI extends AppCompatActivity {
                             mTimer.cancel();
                         }
                         running = 0;
+                        msecond = 0;
+                        mminute = 0;
+                        mhour = 0;
                         Snackbar.make(view, "服务已停止。", Snackbar.LENGTH_LONG)
                                 .setAction("Action", null).show();
                     }
@@ -95,15 +95,42 @@ public class IVI extends AppCompatActivity {
         });
     }
 
+    public Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Log.i("MainActivity", "renewUI");
+                textView3.setText("A.A.A.A.A.A.A.A");
+                textView5.setText("127.0.0.1");
+                textView8.setText("1 M ↑ 2 MB/s");
+                textView10.setText("1 M ↓ 2 MB/s");
+                textView12.setText(String.format("%02d", mhour)+":"+
+                        String.format("%02d", mminute)+":"+
+                        String.format("%02d", msecond));
+                //renewUI();
+            }
+            super.handleMessage(msg);
+        }
+    };
+
     public void timerTask() { //计时器执行的定时任务
         mTimer.schedule(new TimerTask() {
             @Override
             public void run() {
+                msecond ++;
+                if (msecond == 60) {
+                    msecond = 0;
+                    mminute ++;
+                    if (mminute == 60) {
+                        mminute = 0;
+                        mhour ++;
+                    }
+                }
                 if (flag == 0) {
                     int readFlag = readPipe();
                     if (readFlag > 0) {//这里需要修改，判断是否读到了ip地址
                         startVPN();
-                        writePipe();//把虚接口描述符写入管道
+                        //writePipe();//把虚接口描述符写入管道
                         flag = 1;
                     }
                     mHandler.sendEmptyMessage(2);
@@ -116,8 +143,8 @@ public class IVI extends AppCompatActivity {
                     //下载总包数
                     //上传速率（两次总上传流量相减）
                     //下载速率（两次总下载流量相减）
-                    mHandler.sendEmptyMessage(1); //需要刷新ui
                 }
+                mHandler.sendEmptyMessage(1); //需要刷新ui
             }
         }, 1000, 1000);
     }
@@ -131,17 +158,16 @@ public class IVI extends AppCompatActivity {
             BufferedInputStream in = new BufferedInputStream(fileInputStream);
             byte buffer[] = new byte[256];
             try {
-                int readLen = in.read(buffer); //读取管道
+                int readLen = fileInputStream.read(buffer); //读取管道
                 readBuf = buffer;
-                Log.i("MainActivity", "buffer: " + buffer[0]);
-                Log.i("MainActivity", "buffer: " + buffer[1]);
-                Log.i("MainActivity", "buffer: " + buffer[2]);
-                Log.i("MainActivity", "buffer: " + buffer[3]);
-                try {
-                    in.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+                String newmessage = new String(buffer,0, readLen, "US-ASCII");
+                Log.i("MainActivity", "newmessage: " + newmessage);
+                fileInputStream.close();
+//                try {
+//                    in.close();
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
                 return readLen;
             } catch (IOException e){
                 e.printStackTrace();
@@ -172,15 +198,6 @@ public class IVI extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void renewUI() {
-        //根据读取到到值进行转换
-        ipv6Addr.setText("A.A.A.A.A.A.A.A");
-        ipv4Addr.setText("127.0.0.1");
-        uploadInfo.setText("1 M ↑ 2 MB/s");
-        downloadInfo.setText("1 M ↓ 2 MB/s");
-        runTime.setText("00:00:03");
     }
 
     public void startVPN() {
